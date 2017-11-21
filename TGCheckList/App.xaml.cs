ed56306486b138c8a04596a4dd5b7b0c33e6958f
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2017-11-20 23:34:23 -0500

    basic ui for ratings

--- TGCheckList/App.xaml.cs
+++ TGCheckList/App.xaml.cs
@@ -9,16 +9,15 @@ namespace TGCheckList
     {
         public App()
         {
-            AppCenter.Start("ios=186378d1-7745-4667-9791-197f121c5e7b;" + "uwp={Your UWP App secret here};" +
-                   "android={Your Android App secret here}",
-                   typeof(Analytics), typeof(Crashes));
             InitializeComponent();
             MainPage = new TGCheckListPage();
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+             AppCenter.Start("ios=186378d1-7745-4667-9791-197f121c5e7b;" + "uwp={Your UWP App secret here};" +
+                   "android={Your Android App secret here}",
+                   typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
--- TGCheckList/TGCheckListPage.xaml.cs
+++ TGCheckList/TGCheckListPage.xaml.cs
@@ -8,5 +8,21 @@ namespace TGCheckList
         {
             InitializeComponent();
         }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            ratingLabel.Text = $"Pizza rating: {ratingStepper.Value}";
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var pizzaPlace = place.Text;
+            if (string.IsNullOrWhiteSpace(pizzaPlace))
+                return;
+
+            var ratingValue = ratingStepper.Value;
+            var message = $"Your rating {ratingValue} for {pizzaPlace} accepted!";
+            DisplayAlert("Pizza rating", message, "OK");
+        }
     }
 }
